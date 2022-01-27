99d340f35daa5e903ed5f72b69a6a7009b880e4e
Bhuvan Chandra <s544899@nwmissouri.edu>
2022-01-27 13:50:25 -0600

    CalApp

--- BhuvanCalculator/BhuvanCalculator/ViewController.swift
+++ BhuvanCalculator/BhuvanCalculator/ViewController.swift
@@ -12,8 +12,59 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        self.resultLBL.text = ""
     }
 
 
+    @IBOutlet weak var resultLBL: UILabel!
+    
+    var operandOne = 0.0
+    var operandTwo = 0.0
+    var operation = ""
+    
+    @IBAction func eight(_ sender: UIButton) {
+        self.resultLBL.text! += "8"
+        if operandOne == 0.0 {
+            self.operandOne = 8
+        }else{
+            self.operandTwo = 8
+        }
+    }
+    
+    @IBAction func eleven(_ sender: UIButton) {
+        self.resultLBL.text! += "11"
+        if operandTwo == 0.0 {
+            self.operandTwo = 11
+        }else{
+            self.operandOne = 11
+        }
+        
+    }
+    
+    @IBAction func add(_ sender: UIButton) {
+        self.resultLBL.text! +=
+        (sender.titleLabel?.text)!
+        
+        if let _ = self.resultLBL.text{
+            if let buttonTitle = sender.titleLabel?.text{
+                self.resultLBL.text = buttonTitle
+            }
+        }
+        self.operation = "+"
+    }
+    
+    @IBAction func equals(_ sender: UIButton) {
+        if self.operation == "+"{
+            self.resultLBL.text = "\(operandOne + operandTwo)"
+        }
+    }
+    
+    @IBAction func clear(_ sender: UIButton) {
+        self.operandOne = 0.0
+        self.operandTwo = 0.0
+        self.operation = "+"
+        self.resultLBL.text=""
+    }
 }
 
