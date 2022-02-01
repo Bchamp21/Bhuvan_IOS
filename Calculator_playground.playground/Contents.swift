7abaae5dd15f084f4e609634ad6948af733da313
Bhuvan Chandra <s544899@nwmissouri.edu>
2022-02-01 13:49:48 -0600

    modified playground

--- Calculator_playground.playground/Contents.swift
+++ Calculator_playground.playground/Contents.swift
@@ -3,10 +3,19 @@ import UIKit
 var greeting = "Hello, playground"
 
     let stringExp = "3+5/2*6"
-
+s
     let expression = NSExpression(format: stringExp)
 
     let result = expression.expressionValue(with: nil, context: nil) as! Double
     print(result)
+
+
+var str = "Hello World"
+str.removeLast()
+print(str)
+str.removeFirst()
+print(str)
+
+
 
 
