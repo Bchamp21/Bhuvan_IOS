bafb4d6bda9ea60aced480534771dca39a7629f9
Bhuvan Chandra <s544899@nwmissouri.edu>
2022-03-01 13:49:34 -0600

    courseapp

--- Sec05DisplayCourses/Sec05DisplayCourses/ViewController.swift
+++ Sec05DisplayCourses/Sec05DisplayCourses/ViewController.swift
@@ -20,23 +20,60 @@ class ViewController: UIViewController {
         "java" :["title":"Object Oriented Prog in Java","crn":"44542","termOffered":"Fall/Spring","desc":"This is a java course.","image":"java"],
         "ios" :["title":"Mobile Computing-iOS","crn":"44653","termOffered":"Fall/Spring","desc":"This is an iOS app dev course.","image":"ios"]
     ]
-
-    @IBOutlet var courseIconIMG: UIImageView!
-    @IBOutlet var courseTitleLBL: UILabel!
     
-    @IBOutlet var courseCRNLBL: UILabel!
+    lazy var courseIdx = Int(arc4random_uniform(UInt32(self.courses.count)))
+    
+    @IBOutlet weak var courseIconIMG: UIImageView!
+    @IBOutlet weak var courseTitleLBL: UILabel!
+  
+    
+    @IBOutlet weak var courseCRNLBL: UILabel!
     
     @IBOutlet var termOfferedLBL: UILabel!
     
     
     @IBOutlet var courseDescLBL: UILabel!
     
-    @IBAction func previous(_ sender: Any) {
+    @IBAction func previous(_ sender: UIButton) {
+        if courseIdx > 0 {
+            courseIdx -= 1
+        
+            displayCourse(at: courseIdx)
+        }
     }
     
-    @IBAction func next(_ sender: Any) {
+    @IBAction func nextButtonAction(_ sender: Any) {
+//        self.courseTitleLBL.text = "Bhuvan"
+        if courseIdx < self.courses.count-1 {
+//                    courseDescLBL.text = "Bhuvan 01"
+                    courseIdx += 1
+                
+                    displayCourse(at: courseIdx)
+                }
+            }
+
     }
     
+    
+    func displayCourse(at index: Int){
+        
+        let course = Array(self.courses.keys)[index]
+        
+        if let courseModel = self.courses[course]{
+            
+            if let imageName = courseModel["image"]{
+                self.courseIconIMG.image = UIImage(named : imageName)
+            }
+            
+            self.courseTitleLBL.text = courseModel["title"]
+            
+            self.courseCRNLBL.text = courseModel["crn"]
+            
+            self.courseDescLBL.text = courseModel["desc"]
+            
+            self.termOfferedLBL.text = courseModel["termOffered"]
+        }
+    }
     
     
 }
