fdbac392ea2e03d955f2a701549ec749943861ec
Bhuvan Chandra <s544899@nwmissouri.edu>
2022-03-17 13:50:19 -0500

    some new stuff

---
+++ BhuvanAnimation/BhuvanAnimation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BhuvanAnimation
+//
+//  Created by Sarakam,Bhuvan Chandra on 3/17/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBOutlet var myBlueView: UIView!
+    
+    
+    
+    
+}
+
+ 
