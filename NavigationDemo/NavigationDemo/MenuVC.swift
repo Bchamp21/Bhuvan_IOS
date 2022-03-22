d631527876011eb448c20f7cc939a0ea312869c0
Bhuvan Chandra <s544899@nwmissouri.edu>
2022-03-22 13:49:31 -0500

    Navigationbar

---
+++ NavigationDemo/NavigationDemo/MenuVC.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NavigationDemo
+//
+//  Created by Sarakam,Bhuvan Chandra on 3/22/22.
+//
+
+import UIKit
+
+class MenuVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
