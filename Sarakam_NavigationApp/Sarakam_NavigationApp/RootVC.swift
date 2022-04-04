4c0b0a32e2d7c84d70e3b48b7e9f67d50eb7ac1d
Bhuvan Chandra <s544899@nwmissouri.edu>
2022-04-04 09:49:56 -0500

    Assignment05

---
+++ Sarakam_NavigationApp/Sarakam_NavigationApp/RootVC.swift
@@ -0,0 +1,29 @@
+//
+//  RootVC.swift
+//  Sarakam_NavigationApp
+//
+//  Created by Sarakam,Bhuvan Chandra on 4/4/22.
+//
+
+import UIKit
+
+class RootVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Sarakam_NavigationApp/Sarakam_NavigationApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sarakam_NavigationApp
+//
+//  Created by Sarakam,Bhuvan Chandra on 4/4/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
